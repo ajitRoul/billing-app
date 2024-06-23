96acf067c01ecb284a5d32ea85e2f8020ffb6488
Ajit Roul <ajitroul@Ajits-MacBook-Pro.local>
2024-06-23 14:22:56 +0530

    added icon

--- electron.js
+++ electron.js
@@ -1,39 +1,40 @@
-const {app, BrowserWindow} = require('electron')
-    const url = require("url");
-    const path = require("path");
+const { app, BrowserWindow } = require('electron')
+const url = require("url");
+const path = require("path");
 
-    let mainWindow
+let mainWindow
 
-    function createWindow () {
-      mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-          nodeIntegration: true
-        }
-      })
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    fullscreen:true,
+    webPreferences: {
+      nodeIntegration: true,
+      devTools: false
+    },
+    icon: __dirname + '/app_icon.png'
+  })
 
-      mainWindow.loadURL(
-        url.format({
-          pathname: path.join(__dirname, `/dist/billing/browser/index.html`),
-          protocol: "file:",
-          slashes: true
-        })
-      );
-      // Open the DevTools.
-      mainWindow.webContents.openDevTools()
-
-      mainWindow.on('closed', function () {
-        mainWindow = null
-      })
-    }
-
-    app.on('ready', createWindow)
-
-    app.on('window-all-closed', function () {
-      if (process.platform !== 'darwin') app.quit()
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/dist/billing/browser/index.html`),
+      protocol: "file:",
+      slashes: true
     })
+  );
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 
-    app.on('activate', function () {
-      if (mainWindow === null) createWindow()
-    })
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
